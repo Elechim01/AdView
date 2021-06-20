02d537a080b96dff3fc757c7629593f88a0e39cc
Elechim01 <68447158+Elechim01@users.noreply.github.com>
2021-06-20 18:06:36 +0200

    Initial Commit

---
+++ AdView/AdViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdViewApp.swift
+//  AdView
+//
+//  Created by Michele Manniello on 20/06/21.
+//
+
+import SwiftUI
+
+@main
+struct AdViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
